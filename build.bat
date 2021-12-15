7cb51228decfdb94e43c20fb9e7fcc7fcc1161d0
K1tan <76427041+K1tan@users.noreply.github.com>
2021-12-15 20:19:18 +0400

    commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+MD C:\Results\Jenkins
+MD C:\Results\JenkinsTest
+cd C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin
+dotnet restore C:\JenkinsLab
+msbuild.exe C:\Repository\jenkinslab\Jenkins /restore /t:Build /p:OutputPath="C:\Results\Jenkins"
+msbuild.exe C:\Repository\jenkinslab\JenkinsMsTest /restore /t:Build /p:OutputPath="C:\Results\JenkinsTest"
